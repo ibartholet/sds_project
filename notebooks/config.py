78b4bfb8cac5aeb4a27f3978bf4d50ded8a07381
ibartholet <isabellebartholet@bluewin.ch>
2026-05-22 16:52:27 +0200

    udate config

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,4 +1,5 @@
-## Configuration File for Map Key
+## Configuration File for Map Key from FIRMS web page
+# This file was made to easily change map key without having to change the map key in every notebook of this project.
 
-MAP_KEY = "ea49e5fe6beaf3a00954c71727386596"
+MAP_KEY = "ea49e5fe6beaf3a00954c71727386596" #this is my personal map key, please change to your personal map key for further usage
 
